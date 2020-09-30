67ef6cdd9047e64bc2625a8593d7490313a686da
Paul Ambrosiussen <paula@sidefx.com>
2020-09-30 13:18:18 -0700

    [IGNORE] Made unittest work on Linux

--- automation/unittest/run.py
+++ automation/unittest/run.py
@@ -1,32 +1,43 @@
 import os
 import logging
 import subprocess
+import platform
 
 HOUDINI_VERSION = "18.0"
 
 def get_latest_houdini_version():
     logging.info("Determining latest Houdini Version...")
-    sidefx_path = os.path.join(os.getenv("ProgramW6432"), "Side Effects Software")
-    version_list = os.listdir(sidefx_path)
-    version_list.reverse()
-    for possible_dir in version_list:
-        if HOUDINI_VERSION in possible_dir and not "Reality Capture" in possible_dir:
-            logging.info("Latest Local Houdini Install is %s..."%possible_dir)
-            return os.path.join(sidefx_path, possible_dir)
+    machineos = platform.system()
+
+    if machineos == "Windows":
+        sidefx_path = os.path.join(os.getenv("ProgramW6432"), "Side Effects Software")
+        version_list = os.listdir(sidefx_path)
+        version_list.reverse()
+
+        for possible_dir in version_list:
+            if HOUDINI_VERSION in possible_dir and not "Reality Capture" in possible_dir:
+
+                return os.path.join(sidefx_path, possible_dir, "bin", "hython2.7.exe")
+
+    elif machineos == "Linux":
+        sidefx_path = "/opt/hfs{0}/".format(HOUDINI_VERSION)
+
+        return os.path.join(sidefx_path, "bin", "hython2.7")
+
 
 latest_houdini = get_latest_houdini_version()
 local_dir = os.path.dirname(__file__)
+
 my_env = os.environ.copy()
 my_env["HOUDINI_OGL_SOFTWARE"] = "1"
-my_env["HOUDINI_PATH"] = os.path.abspath(os.path.join(os.path.dirname(local_dir), "..")) + ";&" #"C:\\GameDevToolset;&"
+my_env["HOUDINI_PATH"] = os.path.abspath(os.path.join(os.path.dirname(local_dir), "..")) + ";&"
 my_env["PATH"] = os.path.abspath(os.path.join(os.path.dirname(local_dir), "..", "bin")) + ";PATH"
 my_env["SIDEFXLABS"] = os.path.abspath(os.path.join(os.path.dirname(local_dir), ".."))
 
-print latest_houdini
+print latest_houdini, os.path.isfile(latest_houdini)
 
 for filename in os.listdir(local_dir):
-    #print filename
     if "test_" in filename and ".py" in filename:
         print "Running Test:", filename
-        subprocess.call([latest_houdini + "/bin/hython2.7.exe", os.path.join(local_dir, filename)], env=my_env)
+        subprocess.call([latest_houdini, os.path.join(local_dir, filename)], env=my_env)
     
