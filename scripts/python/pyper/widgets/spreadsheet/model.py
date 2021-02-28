28b7d549ab91ebe1ebd5347ef4284c2ad6d716d3
Bruno Ebé <brunoebe@gmail.com>
2021-02-28 14:22:10 +0100

    Removing call to refresh() in setData()

--- scripts/python/pyper/widgets/spreadsheet/model.py
+++ scripts/python/pyper/widgets/spreadsheet/model.py
@@ -126,9 +126,8 @@ class Model(QtCore.QAbstractTableModel):
             if not quiet:
                 self.dataChanged.emit(index, index)
 
-            # we now don't tell the model to refresh itself 
-            # because it should be done by the receiver of the 'dataChanged' signal
-            # self.refresh(self.buildDisplayList())
+            # we don't tell the model to refresh itself
+            # because it should be done by the receiver of the 'dataChanged' signal, either in the view or its parent
 
             return True
 
