44039776e2c97ba6afac09a3261842718c1562c2
Bruno Ebé <brunoebe@gmail.com>
2021-04-22 17:19:06 +0200

    [UPDATE] Updated numberItems tool

--- scripts/python/sidefxedu.py
+++ scripts/python/sidefxedu.py
@@ -57,23 +57,77 @@ def createNotes(kwargs, stickytype="info"):
     sticky.setTextColor(hou.Color(normalize_color(COLOR_TXT[stickytype])))
 
 # This code attaches background images to nodes 
-def numberItems(kwargs=[]):
-    # initialize variables
-    index = 0
-    images = []
+def numberItems(kwargs):
 
-    # get the selected nodes
-    nodes = hou.selectedNodes()
+    ctrlclick = kwargs['ctrlclick']
+    shiftclick = kwargs['shiftclick']
+    altclick = kwargs['altclick']
+    cmdclick = kwargs['cmdclick']
+
     # get the network editor
     editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
-    for node in nodes:
-        image = hou.NetworkImage()
-        # must be full path
-        fullpath = os.path.expandvars('${SIDEFXLABS}/misc/stickers/Edu/SIDEFX_CUSTOM_EMBLEMS_%02d.png' % (index+1))
-        image.setPath(fullpath)
-        image.setRelativeToPath(node.path())
-        image.setRect(hou.BoundingRect(-0.1, 0.1, -1.1, 1.1))
-        index += 1
-        images.append(image)
 
+    # collect already existing images
+    images = []
+    bgimages = editor.backgroundImages()
+    bgimagepaths = [image.path() for image in bgimages]
+    images.extend(bgimages)
+
+    # if CTRL+SHIFT, clear all numbered images
+    index = 1
+    if(ctrlclick & shiftclick):
+        for index in range(1, 30):
+            # define image path (must be full path)
+            fullpath = os.path.expandvars('${SIDEFXLABS}/misc/stickers/Font/SIDEFX_CUSTOM_EMBLEMS %02d.png' % (index+66))
+            # fullpath = os.path.expandvars('${SIDEFXLABS}/misc/stickers/Edu/SIDEFX_CUSTOM_EMBLEMS_%02d.png' % (index))
+
+            # check if the image number is already used
+            try:
+                # if it is, remove it
+                id = bgimagepaths.index(fullpath)
+                images.pop(id)
+                bgimagepaths.pop(id)
+            except:
+                # if not, don't do anything
+                pass
+
+            # set the background images 
+            editor.setBackgroundImages(images)
+        return
+
+    # otherwise create images
+    index = 1
+    for item in hou.selectedItems():
+        # define image path (must be full path)
+        fullpath = os.path.expandvars('${SIDEFXLABS}/misc/stickers/Font/SIDEFX_CUSTOM_EMBLEMS %02d.png' % (index+66))
+        # fullpath = os.path.expandvars('${SIDEFXLABS}/misc/stickers/Edu/SIDEFX_CUSTOM_EMBLEMS_%02d.png' % (index))
+        
+        # check if the image number is already used
+        try:
+            # if it is, remove it
+            id = bgimagepaths.index(fullpath)
+            images.pop(id)
+            bgimagepaths.pop(id)
+        except:
+            # if not, don't do anything
+            pass
+
+        # create a background image and add it to the list
+        image = hou.NetworkImage()
+        image.setPath(fullpath)
+        image.setRelativeToPath(item.path())
+        image.setRect(hou.BoundingRect(-0.1, 0.1, -1.1, 1.1))
+        images.append(image)
+        index += 1
+    
+    # set the background images 
     editor.setBackgroundImages(images)
+
+
+
+
+
+
+
+
+
