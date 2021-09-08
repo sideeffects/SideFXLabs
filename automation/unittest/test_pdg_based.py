10879d8f26bb4665fa2a83912803b771cc140450
Paul Ambrosiussen <paula@sidefx.com>
2021-09-08 13:15:17 -0700

    removed unused imports in pdg tests

--- automation/unittest/test_pdg_based.py
+++ automation/unittest/test_pdg_based.py
@@ -1,41 +1,29 @@
-
 import os
 import hou
-import pdg
-import operator
-
-try:
-	import pdgcmd
-except ImportError:
-	from pdgjob import pdgcmd
-
 
 local_dir = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
 
-	try:
-		hou.hipFile.load(os.path.join(local_dir, "PDG_UnitTest.hip").replace("\\", "/"))
+    try:
+        hou.hipFile.load(os.path.join(local_dir, "PDG_UnitTest.hip").replace("\\", "/"))
 
-		_topnode = hou.node("/obj/topnet1/concatenate_text1")
+        _topnode = hou.node("/obj/topnet1/concatenate_text1")
 
-		print ("starting pdg")
-		_topnode.dirtyAllTasks(False)
-		_topnode.executeGraph(False, True, False, False)
-		print("done with pdg")
+        print ("starting pdg")
+        _topnode.dirtyAllTasks(False)
+        _topnode.executeGraph(False, True, False, False)
+        print("done with pdg")
 
-		# report_resultdata(_topnode.getPDGNode(), True)
+        if os.path.isfile(os.path.join(local_dir, "REGTEST-LOG.txt").replace("\\", "/")):
+            with open(os.path.join(local_dir, "REGTEST-LOG.txt").replace("\\", "/")) as file:
+                lines = file.readlines()
 
+                for line in lines:
+                    print (line)
+        else:
+            print ("ERROR, SOMETHING FAILED IN PDG.. NO LOG GENERATED")
 
-		if os.path.isfile(os.path.join(local_dir, "REGTEST-LOG.txt").replace("\\", "/")):
-			with open(os.path.join(local_dir, "REGTEST-LOG.txt").replace("\\", "/")) as file:
-				lines = file.readlines()
-
-				for line in lines:
-					print (line)
-		else:
-			print ("ERROR, SOMETHING FAILED IN PDG.. NO LOG GENERATED")
-
-	except Exception as e:
-		print (str(e))
-		pass
+    except Exception as e:
+        print (str(e))
+        pass
