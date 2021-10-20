f8ae5a424a1fe08bef1e66a0bbd1df29643285dd
Paul Ambrosiussen <paula@sidefx.com>
2021-10-20 13:37:46 +0200

    Change tests to run on H19

--- automation/unittest/run.py
+++ automation/unittest/run.py
@@ -3,7 +3,7 @@ import logging
 import subprocess
 import platform
 
-HOUDINI_VERSION = "18.5"
+HOUDINI_VERSION = "19.0"
 
 def get_latest_houdini_version():
     logging.info("Determining latest Houdini Version...")
