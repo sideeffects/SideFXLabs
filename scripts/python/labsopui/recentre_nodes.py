d3f575b5d2c6ea8bc8b0ce499232cb518ab19b65
Mai Ao <maia@sidefx.com>
2021-11-14 15:05:53 -0500

    [NEWTOOL] Added a new Network Editor menu utility tool *Recentre with This Node* which will move the current node to the network origin, along with all other items. Updated *Reset Viewport* to allow you to specify the Scene Viewer's external object visibility when you call the function.

---
+++ scripts/python/labsopui/recentre_nodes.py
@@ -0,0 +1,39 @@
+import hou
+
+def recentre_nodes():
+
+    pane_tab = hou.ui.paneTabUnderCursor()
+
+    if type(pane_tab) is hou.NetworkEditor:
+
+        current_node = pane_tab.currentNode()
+
+        network_origin = hou.Vector2(-0.5, -0.15)
+        offset = network_origin.__sub__(current_node.position())
+
+        for item in current_node.parent().allItems():
+
+            # Moves 'item' if it's not inside any NetworkBox
+            if item.parentNetworkBox() is None:
+
+                position = item.position().__add__(offset)
+                item.setPosition(position)
+
+        images = pane_tab.backgroundImages()
+
+        for image in images:
+
+            # Moves 'image' if it's not tied to any item
+            if image.relativeToPath() == '':
+
+                rect = image.rect()
+                bound_min = rect.min().__add__(offset)
+                bound_max = rect.max().__add__(offset)
+                rect.setTo((bound_min.x(), bound_min.y(), bound_max.x(), bound_max.y()))
+                image.setRect(rect)
+
+        pane_tab.setBackgroundImages(images)
+
+        pane_tab.redraw()
+
+    return
