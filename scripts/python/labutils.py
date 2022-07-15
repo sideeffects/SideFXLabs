b978e985a9302f6e26a2fcb56f899cd546c90e38
Mai Ao <maia@sidefx.com>
2022-07-15 18:19:52 -0400

    [UPDATE] Temporarily disables Labs node analytics tracking due to a Python 3.9 deprecation warning spam originated from Houdini's site packages. Wil restore tracking when this is fixed.

--- scripts/python/labutils.py
+++ scripts/python/labutils.py
@@ -52,23 +52,25 @@ def track_event(category, action, label=None, value=0):
             settings.setValue("uuid", hou_uuid)
 
     data = {
-        'v': '1',  # API Version.
-        'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
-        # Anonymous Client Identifier. Ideally, this should be a UUID that
-        # is associated with particular user, device, or browser instance.
-        'cid': hou_uuid,
-        't': 'event',  # Event hit type.
-        'ec': category,  # Event category.
-        'ea': action,  # Event action.
-        'el': label,  # Event label.
-        'ev': value,  # Event value, must be an integer
+       'v': '1',  # API Version.
+       'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
+       # Anonymous Client Identifier. Ideally, this should be a UUID that
+       # is associated with particular user, device, or browser instance.
+       'cid': hou_uuid,
+       't': 'event',  # Event hit type.
+       'ec': category,  # Event category.
+       'ea': action,  # Event action.
+       'el': label,  # Event label.
+       'ev': value,  # Event value, must be an integer
     }
+
+    # Temporarily skips the rest because of a 'collections.abc' Python warning.
+    # Should remove this return when that warning is addressed.
+    return
 
     if requests_enabled:
         try:
-            response = requests.post(
-                'http://www.google-analytics.com/collect', data=data, timeout=0.1)
-
+            response = requests.post('http://www.google-analytics.com/collect', data=data, timeout=0.1)
         except:
             pass
 
