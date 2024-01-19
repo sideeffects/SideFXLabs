2a9ac41a20966f7dac0dc9e5169f7af54f1b66c1
Danicka <danickao@sidefx.com>
2024-01-19 16:18:20 -0600

    [UPDATE] A round of updates to the Biome tools. Python scripts commented with use cases and updated for new workflows

--- scripts/python/biomeutils.py
+++ scripts/python/biomeutils.py
@@ -6,13 +6,29 @@ import json, os
 ###########################################
 
 
+#Useful function to write a JSON-formatted string out to file
+def writeJson(data, file_path):
+    out_file = open(file_path, "w")
+    json.dump(data, out_file, indent = 4, default = lambda d: d.__dict__)    #Serializes as JSON file format
+    out_file.close()
+
+#Useful function to read a JSON-formatted file
+def readJson(file_path):
+    f = open(file_path)
+    data = json.load(f)
+    f.close()
+    return data
+
+
+#Class definition to store biome attributes and later add more
 class Biome:
     def __init__(self, name, aridity_index):
         self.name = name
         self.aridity_index = aridity_index
 
 
-
+#Used by HDAS to read biome profiles and populate dropdown menus with a list of biomes
+#Used by Biome Curve Setup and Biome Initialize in the Biome Name parameters under Menu -> Menu Script
 def biomeNames(biome_json):
     
     biomes = []
@@ -32,18 +48,8 @@ def biomeNames(biome_json):
     return biome_extension
 
 
-def writeJson(data, file_path):
-    out_file = open(file_path, "w")
-    json.dump(data, out_file, indent = 4, default = lambda d: d.__dict__)    #Serializes as JSON file format
-    out_file.close()
-
-def readJson(file_path):
-    f = open(file_path)
-    data = json.load(f)
-    f.close()
-    return data
-
-
+#Sets the default biome settings for newly created files. Will need to be updated with addition of new attributes
+#Used by the readSettingsFile function
 def defaultBiomeSettings(file_path):
     biomes = { "biomes" : [Biome("Tropical Evergreen Forest", 2), Biome("Tropical Seasonal Forest", 8), Biome("Savannah", 4), Biome("Shrubland", 3), 
                 Biome("Semi-desert", 1), Biome("Desert", 6), Biome("Prairie and Steppe", 4), Biome("Temperate Forest", 3), 
@@ -57,8 +63,9 @@ def defaultBiomeSettings(file_path):
     writeJson(biomes, file_path)
 
 
-
-
+#Reads the biome profile JSON and populates HDA multiparm with stored attribute values
+#Used by Biome Profile. Under Scripts ->PythonModule/OnCreated. 
+#In the PythonModule, used for the loadProfile function there. This is accessed in a callback script by multiple HDA parameters
 def readSettingsFile(file_path, hou_pwd):
 
     if not os.path.exists(file_path):
@@ -67,6 +74,8 @@ def readSettingsFile(file_path, hou_pwd):
     biome_settings_data = readJson(file_path)
     biomes = []
 
+    #Next few lines set the multiparm settings on the corresponding Biome Profile HDA. These parameter reference names are hard-coded
+    #Parameters should be added here as more are added to the HDA
     hou_pwd.parm("fd_settings").set(len(biome_settings_data['biomes']))
     count = 1;
 
@@ -76,7 +85,14 @@ def readSettingsFile(file_path, hou_pwd):
         hou_pwd.parm("aridityindex" + str(count)).set(biome_attributes.aridity_index)
         count += 1
 
+
+#Saves the parameters set on the HDA interface as a biome profile JSON
+#Used by Biome Profile. Under Scripts ->PythonModule
+#Used for the saveProfile function there. This is accessed in a callback script on the Save Profile parameter
 def saveSettingsFile(file_path, hou_pwd):
+
+    #Next few lines get the multiparm settings on the corresponding Biome Profile HDA. These parameter reference names are hard-coded
+    #Parameters should be added here as more are added to the HDA
     biome_num =  hou_pwd.parm("fd_settings").eval()
     biome_list = []
     
@@ -95,9 +111,4 @@ def saveSettingsFile(file_path, hou_pwd):
         os.makedirs(os.path.dirname(file_path))
 
     writeJson(biomes, file_path)
-
-def saveFileLocation(file_location, profile_name):
-    new_file_path = file_location + '/biome_settings_location.json'
-    save_file_path = file_location + '/' + profile_name
-    writeJson(save_file_path, new_file_path)
 
