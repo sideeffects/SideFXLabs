3b703498fc632a5f337e25febfa8731a5ef3e9f3
Danicka <danickao@sidefx.com>
2024-02-23 20:42:57 -0600

    [UPDATE] Biome utility library updated to reflect new biome parameters

--- scripts/python/biomeutils.py
+++ scripts/python/biomeutils.py
@@ -22,9 +22,10 @@ def readJson(file_path):
 
 #Class definition to store biome attributes and later add more
 class Biome:
-    def __init__(self, name, aridity_index):
+    def __init__(self, name, average_temp, average_precip):
         self.name = name
-        self.aridity_index = aridity_index
+        self.average_temp = average_temp
+        self.average_precip = average_precip
 
 
 #Used by HDAS to read biome profiles and populate dropdown menus with a list of biomes
@@ -51,10 +52,10 @@ def biomeNames(biome_json):
 #Sets the default biome settings for newly created files. Will need to be updated with addition of new attributes
 #Used by the readSettingsFile function
 def defaultBiomeSettings(file_path):
-    biomes = { "biomes" : [Biome("Tropical Evergreen Forest", 2), Biome("Tropical Seasonal Forest", 8), Biome("Savannah", 4), Biome("Shrubland", 3), 
-                Biome("Semi-desert", 1), Biome("Desert", 6), Biome("Prairie and Steppe", 4), Biome("Temperate Forest", 3), 
-                Biome("Mixed Temperate Forest", 8), Biome("Boreal Evergreen Forest", 5), Biome("Boreal Seasonal Forest", 2),  
-                Biome("Tundra", 8), Biome("Ice", 5), Biome("None", 2)]}
+    biomes = { "biomes" : [Biome("Tropical Evergreen Forest", 25, 3240), Biome("Tropical Seasonal Forest", 25, 2000), Biome("Savannah", 15, 375), Biome("Shrubland", 0, 187), 
+                Biome("Semi-arid", 18, 375), Biome("Desert", 10, 250), Biome("Prairie and Steppe", 0, 600), Biome("Temperate Forest", 10, 1500), 
+                Biome("Mixed Temperate Forest", 5, 1200), Biome("Boreal Evergreen Forest", 0, 400), Biome("Boreal Seasonal Forest", -5, 250),  
+                Biome("Tundra", -10, 350), Biome("Ice", -17, 300), Biome("None", 0, 0)]}
 
     # Create directory if it doesn't exist already
     if not os.path.exists(os.path.dirname(file_path)):
@@ -80,9 +81,10 @@ def readSettingsFile(file_path, hou_pwd):
     count = 1;
 
     for biome in biome_settings_data['biomes']:
-        biome_attributes = Biome(biome['name'], biome['aridity_index'])
+        biome_attributes = Biome(biome['name'], biome['average_temp'], biome['average_precip'])
         hou_pwd.parm("biomename" + str(count)).set(biome_attributes.name)
-        hou_pwd.parm("aridityindex" + str(count)).set(biome_attributes.aridity_index)
+        hou_pwd.parm("avgtemp" + str(count)).set(biome_attributes.average_temp)
+        hou_pwd.parm("avgprecip" + str(count)).set(biome_attributes.average_precip)
         count += 1
 
 
@@ -98,10 +100,11 @@ def saveSettingsFile(file_path, hou_pwd):
     
     for i in range(biome_num):
         i += 1
-        biome_eval = Biome("", 0)
+        biome_eval = Biome("", 0, 0)
 
         biome_eval.name = hou_pwd.parm("biomename" + str(i)).eval()
-        biome_eval.aridity_index = hou_pwd.parm("aridityindex" + str(i)).eval()
+        biome_eval.average_temp = hou_pwd.parm("avgtemp" + str(i)).eval()
+        biome_eval.average_precip = hou_pwd.parm("avgprecip" + str(i)).eval()
         biome_list.append(biome_eval)
 
     biomes = { "biomes" : biome_list} 
